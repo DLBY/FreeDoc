4e30e052afc421b9d66e0b927302da83a83ab9cd
DLBY <david.lobry96@gmail.com>
2020-10-22 15:09:13 +0200

    adding join tables before i destroy everything

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_120509) do
+ActiveRecord::Schema.define(version: 2020_10_22_130559) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
@@ -18,8 +18,16 @@ ActiveRecord::Schema.define(version: 2020_10_22_120509) do
     t.integer "patient_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_appointments_on_city_id"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -28,6 +36,17 @@ ActiveRecord::Schema.define(version: 2020_10_22_120509) do
     t.string "zip_code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_doctors_on_city_id"
+  end
+
+  create_table "join_table_specialties", force: :cascade do |t|
+    t.integer "doctor_id"
+    t.integer "specialty_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["doctor_id"], name: "index_join_table_specialties_on_doctor_id"
+    t.index ["specialty_id"], name: "index_join_table_specialties_on_specialty_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -35,6 +54,17 @@ ActiveRecord::Schema.define(version: 2020_10_22_120509) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_patients_on_city_id"
   end
 
+  create_table "specialties", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "appointments", "cities"
+  add_foreign_key "doctors", "cities"
+  add_foreign_key "patients", "cities"
 end
